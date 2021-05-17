33fcb5e739d87d5225d4f250877a0f60bafc5898
Francesco Cusimano <f.cusimano@student.ie.edu>
2021-05-17 11:30:50 +0200

    Add small test

--- str_utils.py
+++ str_utils.py
@@ -1,2 +1,6 @@
 def tokenize(text):
     return text.split()
+
+
+if __name__ == "__main__": # to only run if I am running the script, but not when I am importing.
+    print(tokenize("Hello world!"))
